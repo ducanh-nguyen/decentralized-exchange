bcb1593576d333621954d7ba5017c3a9373c4c9e
root <root@LON-PF390QMZ-LT.imanage.com>
2023-07-04 12:46:43 +0100

    2. Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  //Fetch contract to be deployed
+  const Token = await ethers.getContractFactory("Token")
+
+  //Deploy contract
+  const token = await Token.deploy()
+  await token.deployed()
+  console.log('Token Deployed to: ${token.address}')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
